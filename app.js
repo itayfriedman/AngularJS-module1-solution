2db722aeb0c6c7e0b92fe9b62ceabc74160d27e3
itayfriedman <itay.friedman@mail.huji.ac.il>
2017-02-20 13:09:01 +0200

    final commit

--- app.js
+++ app.js
@@ -5,6 +5,31 @@ angular.module('LunchCheck', [])
 
 .controller('LunchCheckController', function($scope) {
 	$scope.lunchMenu = "";
+	$scope.message = "";
+	$scope.check =function () {
+		if ($scope.lunchMenu === "") {
+			$scope.message	= "Please enter data first";
+		} else {
+			$scope.message = $scope.lunchMenu;
+			numberDishes($scope.lunchMenu);
+		}
+	};
+
+	function numberDishes (string) {
+		
+		var dishes = string.split(",");
+		var numberDishes = 0;
+		for (var i = 0; i < dishes.length; i++) {
+			if (dishes[i].trim() != "") {
+				numberDishes ++;
+			}
+		}
+		if (numberDishes <= 3) {
+			$scope.message = "Enjoy!";
+		}else {
+			$scope.message = "Too much!";
+		}
+	};
 
 });
 
